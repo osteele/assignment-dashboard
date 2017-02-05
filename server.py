9aec70d11ae7db3fb5b9553772b0c2a0f4fb35a2
Oliver Steele <steele@osteele.com>
2017-02-05 18:32:58 -0500

    scope session to request

--- server.py
+++ server.py
@@ -9,32 +9,27 @@ from models import FileCommit, Repo, Session, User
 
 app = Flask(__name__)
 
-session = Session()
 
-source_repo = session.query(Repo).filter(Repo.source_id == None).first()
-fork_dict = {fork.owner_id: fork for fork in source_repo.forks}  # FIXME abuse of the ORM
+def get_assignment_data():
+    session = Session()
+    source_repo = session.query(Repo).filter(Repo.source_id == None).first()
+    users = session.query(User).all()
 
-users = session.query(User).all()
+    assignment_paths = sorted({file.path for file in source_repo.files if file.path.endswith('.ipynb')})
+    assignment_names = [re.sub(r'day(\d+)_reading_journal\.ipynb', r'Journal #\1', path) for path in assignment_paths]
 
-assignment_paths = sorted({file.path for file in source_repo.files if file.path.endswith('.ipynb')})
-assignment_names = [re.sub(r'day(\d+)_reading_journal\.ipynb', r'Journal #\1', path) for path in assignment_paths]
+    files = session.query(FileCommit).filter(FileCommit.path.in_(assignment_paths)).options(joinedload(FileCommit.repo)).all()
+    user_path_files = {(file.repo.owner_id, file.path): file for file in files}
 
-files = session.query(FileCommit).filter(FileCommit.path.in_(assignment_paths)).options(joinedload(FileCommit.repo)).all()
-user_path_files = {(file.repo.owner_id, file.path): file for file in files}
+    def file_presentation(file, path):
+        if not file:
+            return dict(css_class='danger', path=path, mod_time='missing')
+        return dict(
+            css_class='unchanged' if file.sha == user_path_files[source_repo.owner.id, file.path].sha else None,
+            mod_time=arrow.get(file.mod_time).humanize(),
+            path=path
+        )
 
-
-def file_presentation(file, path):
-    if not file:
-        return dict(css_class='danger', path=path, mod_time='missing')
-    return dict(
-        css_class='unchanged' if file.sha == user_path_files[source_repo.owner.id, file.path].sha else None,
-        mod_time=arrow.get(file.mod_time).humanize(),
-        path=path
-    )
-
-
-@app.route('/')
-def home_page():
     rows = [dict(login=user.login,
                  fullname=user.fullname,
                  repo_url="https://github.com/%s/%s" % (user.login, source_repo.name),
@@ -43,9 +38,19 @@ def home_page():
                  )
             for user in sorted(users, key=lambda u: (u.fullname or u.login).lower())
             if user != source_repo.owner]
+
+    return source_repo, assignment_names, assignment_paths, rows
+
+
+@app.route('/')
+def home_page():
+    source_repo, assignment_names, assignment_paths, rows = get_assignment_data()
     return render_template('index.html',
-                           org_fullname=source_repo.owner.fullname, org_login=source_repo.owner.login, repo_name=source_repo.name,
-                           assignment_names=assignment_names, col_keys=assignment_paths, rows=rows)
+                           org_fullname=source_repo.owner.fullname,
+                           org_login=source_repo.owner.login,
+                           repo_name=source_repo.name,
+                           assignment_names=assignment_names,
+                           col_keys=assignment_paths, rows=rows)
 
 
 if __name__ == '__main__':
