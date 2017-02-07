169522ab17ef27dc9797057b36b05654cd8d9503
Oliver Steele <steele@osteele.com>
2017-02-07 08:55:16 -0500

    download collated notebook

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import re
 from collections import namedtuple
 
 import arrow
-# import nbconvert
-from flask import Flask, redirect, render_template, request, url_for
+import nbformat
+from flask import Flask, make_response, render_template, url_for
 from nbconvert import HTMLExporter
 from sqlalchemy.orm import joinedload
 
 from models import FileCommit, Repo, Session, User
 from nb_combine import nb_combine, safe_read_notebook
+
+PYNB_MIME_TYPE = 'application/x-ipynb+json'
 
 app = Flask(__name__)
 
@@ -51,6 +53,20 @@ def get_assignment_data(session=None):
     return AssignmentData(source_repo=source_repo, assignment_names=assignment_names, assignment_paths=assignment_paths, responses=responses)
 
 
+def get_combined_notebook(assignment_id):
+    session = Session()
+    data = get_assignment_data(session)
+    assignment_path = data.assignment_paths[int(assignment_id)]
+
+    files = session.query(FileCommit).filter(FileCommit.path == assignment_path).options(joinedload(FileCommit.repo)).all()
+    nbs = {file.repo.owner.login: safe_read_notebook(file.file_content.content.decode(), clear_outputs=True)
+           for file in files
+           if file.file_content}
+    owner_nb = nbs[data.source_repo.owner.login]
+    student_nbs = {owner: nb for owner, nb in nbs.items() if owner != data.source_repo.owner.login and nb}
+    return nb_combine(owner_nb, student_nbs)
+
+
 @app.route('/')
 def home_page():
     data = get_assignment_data()
@@ -62,22 +78,6 @@ def home_page():
         assignments=enumerate(data.assignment_names),
         col_keys=data.assignment_paths,
         rows=data.responses)
-
-
-@app.route('/assignment/<assignment_id>/combined')
-def combined_assignment(assignment_id):
-    session = Session()
-    data = get_assignment_data(session)
-    assignment_path = data.assignment_paths[int(assignment_id)]
-
-    files = session.query(FileCommit).filter(FileCommit.path == assignment_path).options(joinedload(FileCommit.repo)).all()
-    nbs = {file.repo.owner.login: safe_read_notebook(file.file_content.content.decode(), clear_outputs=True)
-           for file in files
-           if file.file_content}
-    owner_nb = nbs[data.source_repo.owner.login]
-    student_nbs = {owner: nb for owner, nb in nbs.items() if owner != data.source_repo.owner.login and nb}
-    combined_nb = nb_combine(owner_nb, student_nbs)
-    return HTMLExporter().from_notebook_node(combined_nb)
 
 
 # under development
@@ -93,8 +93,24 @@ def assignment(assignment_id):
         classroom_owner=data.source_repo.owner,
         assignment_name=assignment_name,
         assignment_path=assignment_path,
-        assignment_id=assignment_id
+        collated_html_url=url_for('combined_assignment', assignment_id=assignment_id),
+        collated_nb_download_url=url_for('download_combined_assignment', assignment_id=assignment_id),
     )
+
+
+@app.route('/assignment/<assignment_id>/combined')
+def combined_assignment(assignment_id):
+    combined_nb = get_combined_notebook(assignment_id)
+    return HTMLExporter().from_notebook_node(combined_nb)
+
+
+@app.route('/assignment/<assignment_id>/combined.ipynb')
+def download_combined_assignment(assignment_id):
+    combined_nb = get_combined_notebook(assignment_id)
+    response = make_response(nbformat.writes(combined_nb))
+    response.headers['Content-Disposition'] = 'attachment; filename=assignment-combined.ipynb'
+    response.headers['Content-Type'] = PYNB_MIME_TYPE
+    return response
 
 
 if __name__ == '__main__':
