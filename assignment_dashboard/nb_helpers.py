osteele/assignment-dashboard

commit 8b8de29f98bbb3164fa08a26494379d4eb34b31f

nb_combine → nbcollate
prep for switching to standalone pip package

assignment_dashboard/nbcollate.py
@@ -49,15 +49,15 @@ def nb_clear_outputs(nb):
             cell['outputs'] = []
 
 
-def nb_combine(template_notebook, student_notebooks):
-    nbe = NotebookExtractor(template_notebook, student_notebooks)
+def nbcollate(template_notebook, student_notebooks):
+    nbe = NotebookCollator(template_notebook, student_notebooks)
     return nbe.get_combined_notebook()
 
 
 # The extractor
 #
 
-class NotebookExtractor(object):
+class NotebookCollator(object):
     """The top-level class for extracting answers from a notebook.
 
     TODO: add support multiple notebooks
@@ -117,7 +117,7 @@ class NotebookExtractor(object):
                 suppress_non_answer = bool(prompt.answers)
                 response_cells = \
                     prompt.get_closest_match(notebook_content['cells'],
-                                             NotebookExtractor.MATCH_THRESH,
+                                             NotebookCollator.MATCH_THRESH,
                                              suppress_non_answer)
                 if not response_cells:
                     status = 'missed'

assignment_dashboard/viewmodel.py
@@ -14,8 +14,8 @@ from sqlalchemy.orm import joinedload
 from .database import session
 from .globals import NBFORMAT_VERSION, PYNB_MIME_TYPE
 from .models import Assignment, AssignmentQuestion, AssignmentQuestionResponse, FileCommit, Repo
-from .nb_combine import NotebookExtractor
 from .nb_helpers import safe_read_notebook
+from .nbcollate import NotebookCollator
 
 AssignmentModel = namedtuple('AssignmentModel', 'assignment_path collated_nb answer_status')
 

commit 1f1684a26af02ec4930ae9c2c4754b8d05b060b5

fix errors introduced in 8b8de29

assignment_dashboard/nb_helpers.py
@@ -1,5 +1,7 @@
 # """Jupyter notebook helper functions."""
 
+import nbformat
+
 from .globals import NBFORMAT_VERSION
 
 

assignment_dashboard/nbcollate.py
@@ -162,9 +162,9 @@ class NotebookCollator(object):
 
         answer_book = deepcopy(self.template)
         answer_book['cells'] = filtered_cells
-        if nb_clear_outputs:
+        if clear_outputs:
             answer_book['cells'] = deepcopy(answer_book['cells'])
-            nb_clear_outputs(answer_book['cells'])
+            nb_clear_outputs(answer_book)
         return answer_book
 
     def report_missing_answers(self):

assignment_dashboard/viewmodel.py
@@ -118,7 +118,7 @@ def get_assignment(assignment_id):
         .options(joinedload(FileCommit.file_content)) \
         .filter(FileCommit.path == assignment.path)
 
-    notebooks = {fc.repo.owner.login: safe_read_notebook(fc.content.decode(), clear_outputs=True)
+    notebooks = {fc.repo.owner.login: safe_read_notebook(fc.content.decode())
                  for fc in file_commits
                  if fc.file_content}
 
@@ -126,7 +126,7 @@ def get_assignment(assignment_id):
     assert assignment.repo.owner.login in notebooks, "%s: %s is not in %s" % (assignment.path, assignment.repo.owner.login, notebooks.keys())
     owner_nb = notebooks[assignment.repo.owner.login]
 
-    collation = NotebookExtractor(owner_nb, student_nbs)
+    collation = NotebookCollator(owner_nb, student_nbs)
     answer_status = collation.report_missing_answers()
 
     student_login_ids = {fc.repo.owner.login: fc.repo.owner.id for fc in file_commits}
@@ -141,7 +141,7 @@ def get_assignment(assignment_id):
     assignment.questions = []
     session.commit()
 
-    assignment.nb_content = nbformat.writes(collation.get_combined_notebook())
+    assignment.nb_content = nbformat.writes(collation.get_combined_notebook(clear_outputs=True))
     assignment.questions = questions
     assignment.md5 = files_hash
 