7200fc258164e0a51d142f59ae2d36efd13e498f
Oliver Steele <steele@osteele.com>
2017-02-22 22:36:57 -0500

    move database to ./data

--- assignment_dashboard/database.py
+++ assignment_dashboard/database.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from . import app
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///../database.db')
+DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/database.db'))
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///' + DB_PATH)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 db = SQLAlchemy(app)
