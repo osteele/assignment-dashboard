a96ed9861dc15040302a96572914532cbc3e3731
Oliver Steele <steele@osteele.com>
2017-03-02 09:37:14 -0500

    able to run w/out GitHub app id

--- assignment_dashboard/__init__.py
+++ assignment_dashboard/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa
 
-from assignment_dashboard.app import app
-import assignment_dashboard.commands  # isort:skip
-import assignment_dashboard.views  # isort:skip
 import os
+from assignment_dashboard.app import app
+import assignment_dashboard.commands
+import assignment_dashboard.views
 
-if os.environ.get('GITHUB_CLIENT_ID'):
+if 'GITHUB_CLIENT_ID' in os.environ:
     import assignment_dashboard.oauth
