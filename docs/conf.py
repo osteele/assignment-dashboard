345b99e5b65b5f7f49222ff2ee94118b08a692e3
Oliver Steele <steele@osteele.com>
2017-03-23 22:21:35 -0400

    sphinx: rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,11 @@
 
 from recommonmark.parser import CommonMarkParser
 
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    sphinx_rtd_theme = None
+
 source_parsers = {
     '.md': CommonMarkParser,
 }
@@ -89,6 +94,9 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+if sphinx_rtd_theme:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
